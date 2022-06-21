8b658eb68d90dd6cbe6a9e5bf2bf0fc50b8d93c5
NodjimbaMsa <85159482+NodjimbaMsa@users.noreply.github.com>
2022-06-21 12:58:43 +0200

    Init Projet + Create Migration

---
+++ database/factories/PictureFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Picture;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PictureFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Picture::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'path' => $this->faker->text(255),
+            'product_id' => \App\Models\Product::factory(),
+        ];
+    }
+}
---
+++ database/migrations/2022_06_21_009004_create_product_size_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_size', function (Blueprint $table) {
+            $table
+                ->foreignId('size_id')
+                ->references('id')
+                ->on('sizes')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+
+            $table
+                ->foreignId('product_id')
+                ->references('id')
+                ->on('products')
+                ->onUpdate('CASCADE')
+                ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_size');
+    }
+};
